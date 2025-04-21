ae84d56c80c724b9d2787019864c1c58ae6fd56f
Sam Kortchmar <samkortchmar@gmail.com>
2025-04-21 00:09:35 -0400

    bus

---
+++ main.py
@@ -0,0 +1,34 @@
+from bus_routes_at_location import (
+    get_routes_for_camera,
+    load_route_shapes,
+    load_cameras,
+)
+from runtime import CameraSession
+
+def main():
+    selected_routes = [
+        'M104'
+    ]
+    cameras = load_cameras()
+    route_shapes = load_route_shapes()
+    selected_cameras = []
+
+    for camera in cameras:
+        routes = get_routes_for_camera(camera, route_shapes)
+        if any(route in selected_routes for route in routes):
+            selected_cameras.append(camera)
+
+    sessions = []
+    for camera in selected_cameras:
+        session = CameraSession(camera["id"], camera)
+        sessions.append(session)
+
+    for session in sessions:
+        session.step()
+
+        for track in session.bus_tracks.values():
+            track.dump()
+        
+
+if __name__ == "__main__":
+    main()
